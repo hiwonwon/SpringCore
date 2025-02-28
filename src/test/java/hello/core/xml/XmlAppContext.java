fc19b00279631cf2bfdd8e5ecdd9644ced900fde
hiwonwon <hyewon9913@gmail.com>
2025-02-28 16:32:55 +0900

    섹션5

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package hello.core.xml;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertInstanceOf(MemberService.class, memberService);
+    }
+}
